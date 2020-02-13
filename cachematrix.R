a5d14d6b1f2e82850892cb163de1bebb7184ad43
hyhy20 <60904752+hyhy20@users.noreply.github.com>
2020-02-13 18:04:51 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,5 @@
+## using this function to cathe a matrix, so it well benefits from compute it repeatedly.
+
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
     set <- function(y) {
@@ -13,7 +15,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The function below severs to cathe the inversed matrix, which is created by the function above.
 
 cacheSolve <- function(x, ...) {
     m <- x$getinv()
